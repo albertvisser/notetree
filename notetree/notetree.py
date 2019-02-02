3b5ead5a5e6bc2a0efdb33f76592622261cfbaa9
Albert Visser <albert@lemoncurry>
2019-02-02 19:46:19 +0100

    removed module level code

--- notetree/notetree.py
+++ notetree/notetree.py
@@ -12,9 +12,3 @@ def nt_main(filename, toolkit='qt'):
     elif toolkit == 'qt4':
         from notetree.notetree_qt4 import main
     main(filename)
-
-if __name__ == "__main__":
-    ## fname = 'NoteTree_wx.pck'
-    ## fname = 'NoteTree.pck'
-    fname = 'NoteTree_qt5.pck'
-    nt_main(fname, 'qt')
