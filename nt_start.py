297b05c07dee3e52684854c3bce6c42872fdc8f2
Albert Visser <albert@lemoncurry>
2019-10-26 21:56:27 +0200

    reformatted a little

--- nt_start.py
+++ nt_start.py
@@ -6,7 +6,6 @@ from notetree.main import NoteTree
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Simple Tree Notebook")
-    parser.add_argument("fname", help="Notebook file to use",
-                        default="MyNotes.pck")
+    parser.add_argument('-f', "--fname", help="Notebook file to use", default="MyNotes.pck")
     args = parser.parse_args()
     NoteTree(args.fname)
